7b04589ea747353f7d6d78939f85a25b7265bfeb
Michel Tomas <michel@superbiche.me>
2015-06-15 12:19:50 +0200

    Fixed autoload.php to comply with phpmd & phpcs

--- autoload.php
+++ autoload.php
@@ -1,24 +1,24 @@
 <?php
 spl_autoload_register(function ($class) {
 
-	$namespace_separator = '\\';
+    $namespace_separator = '\\';
 
-	$prefix = 'Itechsup' . $namespace_separator;
+    $prefix = 'Itechsup' . $namespace_separator;
 
-  $base_dir = 'src/Itechsup';
+    $base_dir = 'src/Itechsup';
 
-  $prefix_len = strlen($prefix);
+    $prefix_len = strlen($prefix);
 
-  if (strncasecmp($class, $prefix, $prefix_len) !== 0) {
-    return;
-  }
-  $relative_class = substr($class, $prefix_len);
+    if (strncasecmp($class, $prefix, $prefix_len) !== 0) {
+        return;
+    }
+    $relative_class = substr($class, $prefix_len);
 
-	$relative_file = str_replace($namespace_separator, '/', $relative_class) . '.php';
+    $relative_file = str_replace($namespace_separator, '/', $relative_class) . '.php';
 
-	$file = $base_dir . '/' . $relative_file;
+    $file = $base_dir . '/' . $relative_file;
 
-	if (file_exists($file)) {
-		require_once $file;
-	}
+    if (file_exists($file)) {
+        require_once $file;
+    }
 });
